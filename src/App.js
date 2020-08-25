5939ee50e74857c19ebe89c6c4a0cb9efd684a67
Rahul Kapoor <rahul253801@gmail.com>
2020-08-25 23:02:37 +0530

    added a list and bind with ondismiss , used filter to dismiss item from list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,61 @@
 import React from "react";
 import "./App.css";
 
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list,
+    };
+
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id) {
+    const updatedList = this.state.list.filter((item) => item.objectID !== id);
+    this.setState({
+      list: updatedList,
+    });
+  }
+
   render() {
-    const title = "Road to learn react";
-    return <div className="App">{title}</div>;
+    return (
+      <div className="App">
+        <div className="App-header">
+          Road to learn React
+          {this.state.list.map((item) => (
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <button onClick={() => this.onDismiss(item.objectID)}>
+                Dismiss
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
   }
 }
 
