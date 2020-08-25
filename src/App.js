44f8aaa3627d89f35caf96cbad73304db9dd14b6
Rahul Kapoor <rahul253801@gmail.com>
2020-08-25 20:46:40 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    const title = "Road to learn react";
+    return <div className="App">{title}</div>;
+  }
+}
+
+export default App;
