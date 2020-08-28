0e4e61a2189c0a95b418dd34e8bbf6731df07a0a
Rahul Kapoor <rahul253801@gmail.com>
2020-08-28 21:07:05 +0530

    added few testcases on rtl, enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,95 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import renderer from "react-test-renderer";
+import ReactDOM from "react-dom";
+import App from "./App";
+import Search from "./Components/Search";
+import Button from "./Components/Button";
+import Table from "./Components/Table";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { render, screen } from "@testing-library/react";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+//init the adapter
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("App", () => {
+  it("renders success", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test("rendering app component", () => {
+    render(<App />);
+    //displays skeleton of parent and child components
+    //screen.debug();
+  });
+
+  test("looking up search in dom", () => {
+    render(<App />);
+    expect(screen.getByText("Submit")).toBeInTheDocument();
+  });
+
+  test("has valid snapshot", () => {
+    const component = renderer.create(<App />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe("Search", () => {
+  //it renders search component to dom
+  it("renders success", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Search />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  //it will create a snapshot of search component and match it with a prev snapshot
+  test("has valid snapshot", () => {
+    const component = renderer.create(<Search />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe("Button", () => {
+  it("renders success", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Button />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test("has valid snapshot", () => {
+    const component = renderer.create(<Button />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe("Table", () => {
+  const props = {
+    list: [
+      { title: "1", author: "1", num_comments: 1, points: 2, objectID: "y" },
+      { title: "2", author: "2", num_comments: 1, points: 2, objectID: "z" },
+    ],
+  };
+
+  it("renders success", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Table {...props} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test("has valid snapshot", () => {
+    const component = renderer.create(<Table {...props} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  //enzyme test
+  it("shows items in list", () => {
+    const element = shallow(<Table {...props} />);
+    expect(element.find(".table-row").length).toBe(2);
+  });
 });
